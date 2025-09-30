d057d422415dba33868a610025cb6a631dc276df
ameer hamza <ameer.hamza@upgenicsint.work>
2025-09-30 22:00:51 +0500

    Updated main page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1543,7 +1543,7 @@
         <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
             <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
                 <h1 class="mb-1 font-medium">Let's get started</h1>
-                <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Laravel has an incredibly rich ecosystem. <br>We suggest starting with the following.</p>
+                <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">This is a test for Customize Contact Page</p>
                 <ul class="flex gap-3 text-sm leading-normal">
                     <li>
                         <a href="/contact" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
